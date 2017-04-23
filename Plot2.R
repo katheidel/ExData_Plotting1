f2e57d863074331b6253e934e7a0e4f26c53ef2b
katheidel <katheidel@yahoo.com>
2017-04-22 21:08:01 -0700

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -4,7 +4,9 @@
 setwd("~/Desktop/coursera/exploratory data analysis")
 
 # Read in the data
-hpc <- read.table("household_power_consumption.txt",sep = ";",na.strings = "?",skip=66637,nrows=2880,col.names=c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+hpc <- read.table("household_power_consumption.txt",sep = ";",na.strings = "?",skip=66637,nrows=2880,col.names=
+  c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1",
+  "Sub_metering_2","Sub_metering_3"))
 
 # Create date/time column
 hpc$combinedtime <- paste(hpc$Date, hpc$Time)
